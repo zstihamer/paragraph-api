1695efa0069ce3bd7bdee4867b09ccbdd5ea36db
Tihamer <zstihamer@gmail.com>
2020-03-06 15:46:14 +0100

    config

--- src/main/java/com/github/tzsendej/paginator/ParagraphService.java
+++ src/main/java/com/github/tzsendej/paginator/ParagraphService.java
@@ -1,9 +1,11 @@
 package com.github.tzsendej.paginator;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -25,9 +27,29 @@ public class ParagraphService {
             take = 30;
         }
         for (int i = skip; i <= (skip + take - 1); i++) {
-            data.add(String.format(template, i + 1));
+            data.add(generateRandomData(i + 1));
         }
         return new DataResult(this.total, data);
+    }
+
+    public String generateRandomData(int counter) {
+        int leftLimit = 48;
+        int rightLimit = 122;
+        Random random = new Random();
+        int max = 2000;
+        int min = 100;
+        int targetStringLength = random.nextInt(max + 1 - min) + min;
+
+        String ret = "<div><h6>Paragraph " + counter + "!</h6><br><div><p>" + random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString()
+                + "</p></div></div>";
+
+        // System.out.println(ret);
+        return StringEscapeUtils.escapeHtml4(ret);
+
     }
 }
 
